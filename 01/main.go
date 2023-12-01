b59b807db31e0f442d9e91750c166dbfe30f714a
Mats Estensen <mats.est@gmail.com>
2023-12-01 23:49:06 +0100

    day 01: p2

--- 01/main.go
+++ 01/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"sort"
 	"strconv"
 
 	"github.com/matsest/advent2023/utils"
@@ -37,12 +39,68 @@ func p1(input []string) int {
 	return sum
 }
 
+var stringsToNumbersMap = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+}
+
+func getNumbers2(input string) (numbers []int) {
+
+	// find the index the match is found in
+	numberMap := make(map[int]int)
+	for k, v := range stringsToNumbersMap {
+		m := regexp.MustCompile(k)
+		// consider multiple matches
+		rs := m.FindAllStringIndex(input, -1)
+		for _, r := range rs {
+			if len(r) != 0 {
+				numberMap[r[0]] = v
+			}
+		}
+	}
+	// sort the keys of the map
+	keys := make([]int, 0, len(numberMap))
+	for k := range numberMap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	// make a list of numbers from sorted order
+	for _, k := range keys {
+		numbers = append(numbers, numberMap[k])
+	}
+	return numbers
+}
+
 func p2(input []string) int {
-	return 2
+	sum := 0
+	for _, line := range input {
+		numbers := getNumbers2(line)
+		first := numbers[0]
+		last := numbers[len(numbers)-1]
+		combined := combineNumbers(first, last)
+		sum += combined
+	}
+	return sum
 }
 
 func main() {
 	puzzle_input, _ := utils.ReadLines("./input.txt")
 	fmt.Println(p1(puzzle_input))
-	//fmt.Println(p2(puzzle_input))
+	fmt.Println(p2(puzzle_input))
 }
--- 01/main_test.go
+++ 01/main_test.go
@@ -19,19 +19,30 @@ func Test_p1(t *testing.T) {
 	}
 }
 
-//func Test_p2(t *testing.T) {
-//	tests := []struct {
-//		name      string
-//		args      string
-//		want int
-//	}{
-//		{"default", "args", 2},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			if gotCount := p2(tt.args); gotCount != tt.want {
-//				t.Errorf("p2(%v) = %v, want %v", tt.args, gotCount, tt.want)
-//			}
-//		})
-//	}
-//}
+func Test_p2(t *testing.T) {
+	tests := []struct {
+		name      string
+		args      []string
+		want int
+	}{
+		{"default", []string{
+			"two1nine",
+			"eightwothree",
+			"abcone2threexyz",
+			"xtwone3four",
+			"4nineeightseven2",
+			"zoneight234",
+			"7pqrstsixteen",
+		}, 281},
+		{"multiple", []string{
+			"7tsdxhfrfdkeighteightnine3eight",
+		}, 78},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotCount := p2(tt.args); gotCount != tt.want {
+				t.Errorf("p2(%v) = %v, want %v", tt.args, gotCount, tt.want)
+			}
+		})
+	}
+}
