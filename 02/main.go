d3f5b58248fadda4b3a92ee56e52cbde1b782ebd
Mats Estensen <mats.est@gmail.com>
2023-12-03 01:34:57 +0100

    day 02: p2

--- 02/main.go
+++ 02/main.go
@@ -10,45 +10,57 @@ import (
 
 type Game struct {
 	index int
-	sets []Set
+	sets  []Set
 }
 
 type Set struct {
-	Red int
+	Red   int
 	Green int
-	Blue int
+	Blue  int
 }
 
 var (
-	RED_LIMIT int = 12
+	RED_LIMIT   int = 12
 	GREEN_LIMIT int = 13
-	BLUE_LIMIT int = 14
+	BLUE_LIMIT  int = 14
 )
 
 func possibleGame(game Game) bool {
 	for _, set := range game.sets {
-		if(set.Red > RED_LIMIT || set.Blue > BLUE_LIMIT || set.Green > GREEN_LIMIT){
+		if set.Red > RED_LIMIT || set.Blue > BLUE_LIMIT || set.Green > GREEN_LIMIT {
 			return false
 		}
 	}
 	return true
 }
 
+func powerOfCubes(game Game) int {
+	red, green, blue := 0,0,0
+	for _, set := range game.sets {
+		if set.Red > red {
+			red = set.Red
+		}
+		if set.Green > green {
+			green = set.Green
+		}
+		if set.Blue > blue {
+			blue = set.Blue
+		}
+	}
+	return red * green * blue
+}
+
 func parseInput(lines []string) (games []Game) {
 	for _, line := range lines {
-		//fmt.Println(line)
 		g := Game{}
 		parts := strings.Split(line, ": ")
 		index, _ := strconv.Atoi((strings.Split(parts[0], " ")[1]))
 		g.index = index
 		sets := strings.Split(parts[1], "; ")
-		//fmt.Println(index)
 		for _, set := range sets {
 			newSet := Set{}
 			parts := strings.Split(set, ", ")
-			//fmt.Println(parts)
 			for _, p := range parts {
-				//fmt.Println(p)
 				cubes := strings.Fields(p)
 				cubeCount, _ := strconv.Atoi(cubes[0])
 				cubeColor := cubes[1]
@@ -70,20 +82,23 @@ func parseInput(lines []string) (games []Game) {
 
 func p1(games []Game) (sum int) {
 	for _, g := range games {
-		if possibleGame(g){
+		if possibleGame(g) {
 			sum += g.index
 		}
 	}
 	return sum
 }
 
-func p2(input string) int {
-	return 2
+func p2(games []Game) (sum int) {
+	for _, g := range games {
+		sum += powerOfCubes(g)
+	}
+	return sum
 }
 
-func main(){
+func main() {
 	puzzle_input, _ := utils.ReadLines("input.txt")
 	games := parseInput(puzzle_input)
 	fmt.Println(p1(games))
-	//fmt.Println(p2(puzzle_input))
+	fmt.Println(p2(games))
 }
--- 02/main_test.go
+++ 02/main_test.go
@@ -24,20 +24,3 @@ func Test_possibleGame(t *testing.T) {
 		})
 	}
 }
-
-func Test_p2(t *testing.T) {
-	tests := []struct {
-		name string
-		args string
-		want int
-	}{
-		{"default", "args", 2},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if gotCount := p2(tt.args); gotCount != tt.want {
-				t.Errorf("p2(%v) = %v, want %v", tt.args, gotCount, tt.want)
-			}
-		})
-	}
-}
