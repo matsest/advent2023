e61475b5972a3325c22d5c21ca0bd84eb720b7ce
Mats Estensen <mats.est@gmail.com>
2023-12-03 01:46:44 +0100

    day 02: test p2

--- 02/main_test.go
+++ 02/main_test.go
@@ -24,3 +24,24 @@ func Test_possibleGame(t *testing.T) {
 		})
 	}
 }
+
+func Test_powerOfCubes(t *testing.T) {
+	tests := []struct {
+		name string
+		args Game
+		want int
+	}{
+		{"possible1", Game{1, []Set{{4, 0, 3}, {1, 2, 6}, {0, 2, 0}}}, 48},
+		{"possible2", Game{2, []Set{{0, 2, 1}, {1, 3, 4}, {0, 1, 1}}}, 12},
+		{"impossible", Game{3, []Set{{20, 8, 6}, {4, 13, 5}, {1, 5, 0}}}, 1560},
+		{"impossible2", Game{4, []Set{{3, 1, 6}, {6, 3, 0}, {14, 3, 15}}}, 630},
+		{"possible3", Game{5, []Set{{6, 3, 1}, {1, 2, 2}}}, 36},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := powerOfCubes(tt.args); got != tt.want {
+				t.Errorf("powerOfCubes(%v) = %v, want %v", tt.args, got, tt.want)
+			}
+		})
+	}
+}
